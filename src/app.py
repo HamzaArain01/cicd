ac16568c56280a4d5f215e71aa96c0f92d822fdb
HamzaArain01 <hamza.hanif@8thloop.com>
2022-10-15 20:27:32 +0500

    deploy heroku changes

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def index():
-    return "Hello World"
+    return "Hello, world!"
+
 
 if __name__ == "__main__":
     app.run()
